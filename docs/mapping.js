ab9dfbfa321e11f60fb460ab717525b13b6e3e41
みゅる <hiruryu02@gmail.com>
2025-03-27 17:20:33 +0900

    Update mapping.js

--- docs/mapping.js
+++ docs/mapping.js
@@ -1,6 +1,6 @@
 var linkMapping = {
-    "付称辞": "petnosciuk",
-    "格": "petnosciuk",
-    "付称包辞": "petnoxek",
+    "付称辞": "petnosciuk⚠️",
+    "格": "petnosciuk⚠️",
+    "付称包辞": "petnoxek⚠️",
     // 他のマッピングもここに記述
   };
