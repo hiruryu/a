5465b2e533f4fa03c766247c837703c2c31a2e93
みゅる <hiruryu02@gmail.com>
2025-04-08 15:20:04 +0900

    Update mapping.js

--- docs/mapping.js
+++ docs/mapping.js
@@ -1,7 +1,7 @@
 var linkMapping = {
-    "付称辞": "petnosciuk⚠️",
-    "格": "petnosciuk⚠️",
-    "付称包辞": "petnoxek⚠️",
+    "付称辞": "petnasciuk⚠️",
+    "格": "petnasciuk⚠️",
+    "付称包辞": "petnaxek⚠️",
     "人格性": "áskesta⚠️",
     // 他のマッピングもここに記述
   };
